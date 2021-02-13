94c938ac3d344bc3885332ab9667c6d33bf18b33
Nivaldo Carvalho <nivaldo_j@outlook.com>
2021-02-12 22:56:27 -0300

    add: empty api token constants

---
+++ app/src/main/java/nivaldo/dh/exercise/webservices/shared/constant/ApiTokenConstants.kt
@@ -0,0 +1,8 @@
+package nivaldo.dh.exercise.webservices.shared.constant
+
+object ApiTokenConstants {
+
+    const val PUBLIC_KEY = "INSERT API KEY HERE"
+    const val PRIVATE_KEY = "INSERT API KEY HERE"
+
+}
